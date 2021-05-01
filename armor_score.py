6076e42e2277d623225c9ea745a80597fb554d0e
qui3xote <tmellis@gmail.com>
2021-05-01 14:41:41 -0400

    Added comments

--- armor_score.py
+++ armor_score.py
@@ -9,18 +9,14 @@ REQUIRED = ['Name','Hash','Tier','Type','Equippable','Mobility (Base)',
     'Intellect (Base)','Strength (Base)','Total (Base)']
 STATS = ['Mobility (Base)', 'Recovery (Base)','Discipline (Base)',
     'Intellect (Base)','Strength (Base)','Total (Base)']
-VALID_TYPES = ['Helmet','Chest Armor','Gauntlets','Leg Armor']
+ARMOR_TYPES = ['Helmet','Chest Armor','Gauntlets','Leg Armor']
+
 # group = parser.add_mutually_exclusive_group()
 # group.add_argument("-v", "--verbose", action="store_true")
 # group.add_argument("-q", "--quiet", action="store_true")
 #parser.add_argument("x", type=int, help="the base")
 #parser.add_argument("y", type=int, help="the exponent")
 
-# args = {}
-# args['filename'] = './destinyArmor.csv'
-# args['limit'] = None
-# args['outputfile'] = './test.csv'
-# character = 'Hunter'
 
 def main():
     parser = argparse.ArgumentParser(description="Score armor for potential usefulness")
@@ -35,68 +31,76 @@ def main():
         if r not in df.columns.values:
             print("Input file is missing required column: {}".format(r))
             print("Use ArmorScore.py -h to view list of required columns")
-            #return
+            return
     if args.clear is True:
         df['Notes'] = ''
 
     df['score'] = pd.Series([0.0 for x in range(len(df.index))], index=df.index)
-    df = df[df['Type'].isin(VALID_TYPES)]
+    df = df[df['Type'].isin(ARMOR_TYPES)] #Get rid of class armor
 
     characters = df['Equippable'].unique().tolist()
-    combine_columns = []
+    combined_stat_columns = []
+    
+    #Generate all possible combinations of stats
     for n in range(2,7):
-        combine_columns.append(list(set([tuple(sorted(x)) for x in itertools.permutations(STATS,n)])))
+        combined_stat_columns.append(list(set([tuple(sorted(x)) for x in itertools.permutations(STATS,n)])))
 
-    combine_columns = [list(item) for sublist in combine_columns for item in sublist]
-    combine_column_names = [" | ".join(x) for x in combine_columns]
+    combined_stat_columns = [list(item) for sublist in combined_stat_columns for item in sublist]
+    combined_stat_column_names = [" | ".join(x) for x in combined_stat_columns]
 
-
-    armor_frames = []
 
     for character in characters:
         df.set_index('Id',inplace=True)
-        print('Scoring {num} {char} armor pieces'.format(num=df[df['Equippable']==character].shape[0],char=character))
+        print('Scoring {num} {char} armor pieces'.format(num=df[df['Equippable']==character].shape[0], char=character))
         start = time.time()
-        armor = {t:df.loc[(df['Equippable']==character) & (df['Type']==t)].index.values.tolist() for t in VALID_TYPES}
-        armor_df = pd.DataFrame(list(itertools.product(*armor.values())), columns=[k for k in armor.keys()])
+        
+        #Get all the armor permutations and create a dataframe
+        armor = {t:df.loc[(df['Equippable']==character) & (df['Type']==t)].index.values.tolist() for t in ARMOR_TYPES}
+        armor_permutations_df = pd.DataFrame(list(itertools.product(*armor.values())), columns=[k for k in armor.keys()])
 
 
         if args.limit is not None:
-            armor_df = armor_df.loc[[x for x in range(args.limit)]]
+            armor_permutations_df = armor_permutations_df.loc[[x for x in range(args.limit)]]
 
-        stat_sums = pd.DataFrame(columns=STATS)
-
-        combine_df = pd.DataFrame(columns=combine_column_names)
-
+        #Getthe stats for each armor piece for each permutation.
         frames = []
-        for type in VALID_TYPES:
-            frames.append(armor_df.set_index(type).join(df[STATS]).reset_index())
-
-        for d in zip(frames,VALID_TYPES):
+        for type in ARMOR_TYPES:
+            frames.append(armor_permutations_df.set_index(type).join(df[STATS]).reset_index())
+            
+        #Is there a better way to prevent index resetting from losing the name?
+        for d in zip(frames,ARMOR_TYPES):
             d[0].rename(columns={'index':d[1]},inplace=True)
 
-        armor_df = pd.concat(frames)
-        armor_df = armor_df.groupby(VALID_TYPES).sum().reset_index()
+        #Recombine indivudal armor dfs and use groupby to sum into final loadout stats
+        armor_permutations_df = pd.concat(frames)
+        armor_permutations_df = armor_permutations_df.groupby(ARMOR_TYPES).sum().reset_index()
 
-        for c in zip(combine_columns,combine_column_names):
-            combine_df[c[1]] = armor_df[c[0]].sum(axis=1)
+        #build a df with one column for every stat combination and fill in with the sums from armor_permutations
+        #This can probably be done without a seperate dataframe
+        combine_df = pd.DataFrame(columns=combined_stat_column_names)
+        for c in zip(combined_stat_columns,combined_stat_column_names):
+            combine_df[c[1]] = armor_permutations_df[c[0]].sum(axis=1)
 
-        armor_df = pd.concat([armor_df, combine_df],axis=1)
+        #Append the combined stats on tto he armor permutations.
+        armor_permutations_df = pd.concat([armor_permutations_df, combine_df],axis=1)
 
-        for col in STATS + combine_column_names:
-            armor_df[col] = pd.to_numeric(armor_df[col])
+        #not sure why these aren't numeric to start with, but what's another for loop between friends?
+        for col in STATS + combined_stat_column_names:
+            armor_permutations_df[col] = pd.to_numeric(armor_permutations_df[col])
 
+        #Get all the armor pieces in each winning permutation
         winning_ids = []
-        for stat in STATS + combine_column_names:
-            winning_ids.append(armor_df.loc[armor_df[stat].idxmax()][VALID_TYPES].values.tolist())
+        for stat in STATS + combined_stat_column_names:
+            winning_ids.append(armor_permutations_df.loc[armor_permutations_df[stat].idxmax()][ARMOR_TYPES].values.tolist())
 
+        #Add up the scores and put them back on the original data frame
         df.reset_index(inplace=True)
         for i in [item for sublist in winning_ids for item in sublist]:
-            df.iloc[df.index[df['Id']==i],df.columns.get_loc('score')] += 1/len(STATS + combine_column_names)
+            df.iloc[df.index[df['Id']==i],df.columns.get_loc('score')] += 1/len(STATS + combined_stat_column_names)
 
+        print('{num} {char} armor combinations scored in {seconds} seconds'.format(num=armor_permutations_df.shape[0], char=character, seconds = time.time() - start))
 
-        print('{num} {char} armor combinations scored in {seconds} seconds'.format(num=armor_df.shape[0], char=character, seconds = time.time() - start))
-
+    #Put the scores into the notes column and write it out to file....
     df['score'] = df['score'].round(2)
     df['score'] = "{ArmorScore: " + df['score'].astype(str) + "}"
     df['Notes'] = df['Notes'].fillna('').astype(str)
