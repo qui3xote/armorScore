8c6e4e325f782e42fc9468257accf7422a81b2c2
qui3xote <tmellis@gmail.com>
2021-05-02 06:54:47 -0400

    Much cleanup of var names and randomly assigned but not used variables...

--- armor_score.py
+++ armor_score.py
@@ -20,25 +20,42 @@ ARMOR_TYPES = ['Helmet','Chest Armor','Gauntlets','Leg Armor']
 
 def main():
     parser = argparse.ArgumentParser(description="Score armor for potential usefulness")
-    parser.add_argument("-f", "--filename", type=str, help="DIM export file to be processed.")
+    parser.add_argument("filename", type=str, help="DIM export file to be processed.")
     parser.add_argument("-o", "--outputfile", type=str, default=None, help="Where to store the results.")
-    parser.add_argument("-l", "--limit", type=int, default=None, help="For testing only. Limit number of rows iterrated")
+    parser.add_argument("-b", "--includeblue", action="store_true", default=False,
+                        help="Incude uncommon items in the scoring.")
+    parser.add_argument("-e", "--exotics", type=str, choices=['pin','prefer','ignore'], default='prefer',
+                        help="""If 'pin', only look at loadouts that include an exotic. If 'ignore'. look at all valid 
+                        loadouts and pick the highest stats. If 'prefer', run both modes and combine the scores. By 
+                        default, armorScore runs 'prefer'.""" )
+    parser.add_argument("-p", "--pinstats", type=str, default=[], nargs='+', choices=['I','M','D','S','Rc','Rs'],
+                        help="Only consider stat combinations that include the chosen stat(s).")
+    parser.add_argument("-l", "--limit", type=int, default=None, help="For testing only. Limit number of rows iterated")
     parser.add_argument("-c", "--clear", action="store_true", default=False, help="For testing only. Clear notes column.")
     args = parser.parse_args()
 
-    df = pd.read_csv(args.filename)
+    dim_df = pd.read_csv(args.filename)
     for r in REQUIRED:
-        if r not in df.columns.values:
+        if r not in dim_df.columns.values:
             print("Input file is missing required column: {}".format(r))
             print("Use ArmorScore.py -h to view list of required columns")
             return
+
     if args.clear is True:
-        df['Notes'] = ''
+        dim_df['Notes'] = ''
 
-    df['score'] = pd.Series([0.0 for x in range(len(df.index))], index=df.index)
-    df = df[df['Type'].isin(ARMOR_TYPES)] #Get rid of class armor
+    if args.includeblue is True:
+        rarity = ['Uncommon', 'Legendary', 'Exotic']
+    else:
+        rarity = ['Legendary', 'Exotic']
 
-    characters = df['Equippable'].unique().tolist()
+    # Get rid of class armor & lower tier items
+    dim_df = dim_df[(dim_df['Type'].isin(ARMOR_TYPES)) & (dim_df['Tier'].isin(rarity))]
+
+    dim_df['score'] = pd.Series([0.0 for x in range(len(dim_df.index))], index=dim_df.index)
+
+
+    characters = dim_df['Equippable'].unique().tolist()
     combined_stat_columns = []
     
     #Generate all possible combinations of stats
@@ -50,12 +67,12 @@ def main():
 
 
     for character in characters:
-        df.set_index('Id',inplace=True)
-        print('Scoring {num} {char} armor pieces'.format(num=df[df['Equippable']==character].shape[0], char=character))
+        dim_df.set_index('Id', inplace=True)
+        print('Scoring {num} {char} armor pieces'.format(num=dim_df[dim_df['Equippable'] == character].shape[0], char=character))
         start = time.time()
         
         #Get all the armor permutations and create a dataframe
-        armor = {t:df.loc[(df['Equippable']==character) & (df['Type']==t)].index.values.tolist() for t in ARMOR_TYPES}
+        armor = {t:dim_df.loc[(dim_df['Equippable'] == character) & (dim_df['Type'] == t)].index.values.tolist() for t in ARMOR_TYPES}
         armor_permutations_df = pd.DataFrame(list(itertools.product(*armor.values())), columns=[k for k in armor.keys()])
 
 
@@ -65,7 +82,7 @@ def main():
         #Getthe stats for each armor piece for each permutation.
         frames = []
         for type in ARMOR_TYPES:
-            frames.append(armor_permutations_df.set_index(type).join(df[STATS]).reset_index())
+            frames.append(armor_permutations_df.set_index(type).join(dim_df[STATS]).reset_index())
             
         #Is there a better way to prevent index resetting from losing the name?
         for d in zip(frames,ARMOR_TYPES):
@@ -94,19 +111,19 @@ def main():
             winning_ids.append(armor_permutations_df.loc[armor_permutations_df[stat].idxmax()][ARMOR_TYPES].values.tolist())
 
         #Add up the scores and put them back on the original data frame
-        df.reset_index(inplace=True)
+        dim_df.reset_index(inplace=True)
         for i in [item for sublist in winning_ids for item in sublist]:
-            df.iloc[df.index[df['Id']==i],df.columns.get_loc('score')] += 1/len(STATS + combined_stat_column_names)
+            dim_df.iloc[dim_df.index[dim_df['Id'] == i], dim_df.columns.get_loc('score')] += 1 / len(STATS + combined_stat_column_names)
 
         print('{num} {char} armor combinations scored in {seconds} seconds'.format(num=armor_permutations_df.shape[0], char=character, seconds = time.time() - start))
 
     #Put the scores into the notes column and write it out to file....
-    df['score'] = df['score'].round(2)
-    df['score'] = "{ArmorScore: " + df['score'].astype(str) + "}"
-    df['Notes'] = df['Notes'].fillna('').astype(str)
-    df['Notes'] = df['score'] + df['Notes']
+    dim_df['score'] = dim_df['score'].round(2)
+    dim_df['score'] = "{ArmorScore: " + dim_df['score'].astype(str) + "}"
+    dim_df['Notes'] = dim_df['Notes'].fillna('').astype(str)
+    dim_df['Notes'] = dim_df['score'] + dim_df['Notes']
 
-    df.to_csv(args.outputfile)
+    dim_df.to_csv(args.outputfile)
 
 
 if __name__ == '__main__':
